e2aa3b8703bc60a1729540fd62827c469ea28d93
HereticInquisitor <ayushraj7353@gmail.com>
2023-12-05 22:14:06 +0530

    first

---
+++ empty.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+connected_monitors=$(xrandr | grep -w "connected" | grep -v "eDP" | awk '{print $1}')
+gpumode=$(envycontrol -q)
+status=$(acpi -b | awk '{print $3}')
+
+if [ -n "$connected_monitors" ]; then
+
+    if [ "$gpumode" = "integrated" ]; then
+    
+        echo -e "External monitor(s) connected\nCurrently the system is in $gpumode mode"
+        if [[ "$status" == *Charging* || "$status" == *Not* ]]; then
+            echo "Laptop is plugged in. Enter sudo password to change GPU mode:"
+            sudo envycontrol -s hybrid
+            echo "GPU mode changed to hybrid. Initiating system reboot."
+            sudo shutdown -r now
+            
+        elif [[ "$status" == *Discharging* ]]; then
+            echo "Laptop is not plugged in. Press 1 to enter sudo password, 2 to skip:"
+            read -p "Enter your choice: " num
+            
+            if [ "$num" = 1 ]; then
+                sudo envycontrol -s hybrid
+                echo "GPU mode changed to hybrid. Initiating system reboot."
+                sudo shutdown -r now
+
+            elif [ "$num" = 2 ]; then
+                echo "GPU mode is in Integrated."
+            fi
+        fi
+        
+    elif [ "$gpumode" = "hybrid" ]; then
+        echo -e "External monitor(s) connected.\nCurrently the system is in $gpumode mode"
+        
+        if [[ "$status" == *Discharging* ]]; then
+            echo "Laptop is not plugged in. Press 1 to enter sudo password, 2 to skip:"
+            read -p "Enter your choice: " num
+            
+            if [ "$num" = 1 ]; then
+                sudo envycontrol -s integrated
+                echo "GPU mode changed to integrated. Initiating system reboot."
+                sudo shutdown -r now
+                
+            elif [ "$num" = 2 ]; then
+                echo "GPU mode is in hybrid."
+            fi
+        fi
+    fi
+    
+    
+else
+
+    if [ "$gpumode" = "hybrid" ]; then
+        echo "No external monitor(s) connected."
+        
+        if [[ "$status" == *Discharging* ]]; then
+            echo "Laptop is not plugged in. Enter sudo password to change GPU mode:"
+            sudo envycontrol -s integrated
+            echo "GPU mode changed to integrated. Initiating system reboot."
+            sudo shutdown -r now
+            
+        elif [[ "$status" == *Charging* || "$status" == *Not* ]]; then
+            echo "Laptop is plugged in. Press 1 to enter sudo password, 2 to skip:"
+            read -p "Enter your choice: " nums
+            
+            if [ "$nums" = 1 ]; then
+                sudo envycontrol -s integrated
+                echo "GPU mode changed to integrated. Initiating system reboot."
+                sudo shutdown -r now
+                
+            elif [ "$nums" = 2 ]; then
+                echo "GPU mode is in hybrid."
+            fi
+        fi
+    fi
+fi
+
