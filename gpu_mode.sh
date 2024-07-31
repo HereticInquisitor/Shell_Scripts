ca7e2600c33e381e5f8a36e76c4725d757a17e10
HereticInquisitor <ayushraj7353@gmail.com>
2024-08-01 00:05:13 +0530

    shutdown--> reboot

--- gpu_mode.sh
+++ gpu_mode.sh
@@ -13,7 +13,7 @@ if [ -n "$connected_monitors" ]; then
             echo "Laptop is plugged in. Enter sudo password to change GPU mode:"
             sudo envycontrol -s hybrid
             echo "GPU mode changed to hybrid. Initiating system reboot."
-            sudo shutdown -r now
+            sudo reboot
             
         elif [[ "$status" == *Discharging* ]]; then
             echo "Laptop is not plugged in. Press 1 to enter sudo password, 2 to skip:"
@@ -22,7 +22,7 @@ if [ -n "$connected_monitors" ]; then
             if [ "$num" = 1 ]; then
                 sudo envycontrol -s hybrid
                 echo "GPU mode changed to hybrid. Initiating system reboot."
-                sudo shutdown -r now
+                sudo reboot
 
             elif [ "$num" = 2 ]; then
                 echo "GPU mode is in Integrated."
@@ -39,7 +39,7 @@ if [ -n "$connected_monitors" ]; then
             if [ "$num" = 1 ]; then
                 sudo envycontrol -s integrated
                 echo "GPU mode changed to integrated. Initiating system reboot."
-                sudo shutdown -r now
+                sudo reboot
                 
             elif [ "$num" = 2 ]; then
                 echo "GPU mode is in hybrid."
@@ -57,7 +57,7 @@ else
             echo "Laptop is not plugged in. Enter sudo password to change GPU mode:"
             sudo envycontrol -s integrated
             echo "GPU mode changed to integrated. Initiating system reboot."
-            sudo shutdown -r now
+            sudo reboot
             
         elif [[ "$status" == *Charging* || "$status" == *Not* ]]; then
             echo "Laptop is plugged in. Press 1 to enter sudo password, 2 to skip:"
@@ -66,7 +66,7 @@ else
             if [ "$nums" = 1 ]; then
                 sudo envycontrol -s integrated
                 echo "GPU mode changed to integrated. Initiating system reboot."
-                sudo shutdown -r now
+                sudo reboot
                 
             elif [ "$nums" = 2 ]; then
                 echo "GPU mode is in hybrid."
--- run_Script.bat
+++ run_Script.bat
@@ -1,5 +1,5 @@
 REM This is only for Windows 
-@REM  Work in progress doesn't work properly , make this work properly if possible
+@REM  Work in progress, doesn't work properly , make this work properly if possible
 @echo off
 python path\to\your\script.py
 pause
